a3c92e818c207dfd6604b3971923691675765dcd
Ксю <@DESKTOP-J0L330U>
2020-10-09 19:35:04 +0500

    classes added

---
+++ Domain/ScriptHistory/8.sql
@@ -0,0 +1,11 @@
+﻿exec Check_db_Version 8
+
+update HtmlBlocks
+set ParentCSSClass = 'who-we-are' , ChildCSSClass = 'purchase_block_in'
+where SiteBlockId = '4DBAD896-75A2-46CB-A9BA-9E1B40915884'
+
+update HtmlBlocksChildren
+set CSSClass = 'purchase_in'
+where SiteBlockId = '4DBAD896-75A2-46CB-A9BA-9E1B40915884'
+
+exec Update_db_Version 8
