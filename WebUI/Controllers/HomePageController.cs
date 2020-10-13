efa0687c7775bf2b194369dca77e5235ae7906bc
Dancelaim <49374348+Dancelaim@users.noreply.github.com>
2020-10-13 22:15:06 +0500

    Cart pop Up

--- WebUI/Controllers/HomePageController.cs
+++ WebUI/Controllers/HomePageController.cs
@@ -11,23 +11,12 @@ namespace WebUI.Controllers
 {
     public class HomePageController : Controller
     {
-        private IProductRepository repository;
-        public HomePageController(IProductRepository repo)
-        {
-            repository = repo;
-        }
         // GET: HomePage
         public ActionResult Home()
         {
             @ViewBag.currentGame = "Select Game";
             Session["SelectedGame"] = null;
             return View();
-        }
-        public ActionResult PopUp()
-        {
-            CartController cartController = new CartController(repository);
-            
-            return View(cartController.CartPopUp());
         }
     }
 }
