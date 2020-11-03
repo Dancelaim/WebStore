ac468e94898ad540fd639bdd7172fc45b2e8c00d
Ксю <@DESKTOP-J0L330U>
2020-11-03 21:52:03 +0500

    fix admin styles

--- Domain/Entities/ProductDetails.cs
+++ Domain/Entities/ProductDetails.cs
@@ -30,6 +30,8 @@ namespace WowCarry.Domain.Entities
         public bool InStock { get { return product.InStock; } }
 
         public bool PreOrder { get { return product.PreOrder; } }
+
+        [Display(Name = "Status")]
         public bool ProductEnabled { get { return product.ProductEnabled; } }
 
         [Display(Name = "Product Quantity")]
@@ -41,27 +43,62 @@ namespace WowCarry.Domain.Entities
         [Display(Name = "Large Image")]
         public string ProductImage { get { return product.ProductImage; } }
 
-        [Display(Name = "Status")]
-        public bool ProductEnabled { get { return product.ProductEnabled; } }
+        [Display(Name = "Sort Order")]
         public int? ProductPriority { get { return product.ProductPriority; } }
+
+        [Display(Name = "Category name")]
         public string ProductCategoryName { get { return product.ProductCategory.ProductCategoryName; } }
+
+        [Display(Name = "SubCategory name")]
         public string ProductSubCategoryName { get; set; }
+
+        [Required(ErrorMessage = "Game name is required")]
+        [Display(Name = "Game name")]
         public string ProductGameName { get { return product.ProductGame.GameName; } }
+
+        [Display(Name = "Price EU")]
         public decimal? ProductPriceEU { get { return product.ProductPrice.Where(p=>p.Region=="EU").Select(p=>p.Price).FirstOrDefault(); } }
+
+        [Display(Name = "Price US")]
         public decimal? ProductPriceUS { get { return product.ProductPrice.Where(p => p.Region == "US").Select(p => p.Price).FirstOrDefault(); } }
+
+        [Display(Name = "Sale EU")]
         public decimal? ProductSaleEU { get { return product.ProductPrice.Where(p => p.Region == "EU").Select(p => p.ProductSale).FirstOrDefault();} }
+
+        [Display(Name = "Sale US")]
         public decimal? ProductSaleUS { get { return product.ProductPrice.Where(p => p.Region == "US").Select(p => p.ProductSale).FirstOrDefault(); } }
 
+        [Display(Name = "Product Description")]
         public string Description { get { return product.ProductDescription.Description; } }
+
+        [Display(Name = "Product SubDescription Title 1")]
         public string SubDescriptionTitle1 { get { return product.ProductDescription.SubDescriptionTitle1; } }
+
+        [Display(Name = "Product SubDescription 1")]
         public string SubDescription1 { get { return product.ProductDescription.SubDescription1; } }
+
+        [Display(Name = "Product SubDescription Title 2")]
         public string SubDescriptionTitle2 { get { return product.ProductDescription.SubDescriptionTitle2; } }
+
+        [Display(Name = "Product SubDescription 2")]
         public string SubDescription2 { get { return product.ProductDescription.SubDescription2; } }
+
+        [Display(Name = "Product SubDescription Title 3")]
         public string SubDescriptionTitle3 { get { return product.ProductDescription.SubDescriptionTitle3; } }
+
+        [Display(Name = "Product SubDescription 3")]
         public string SubDescription3 { get { return product.ProductDescription.SubDescription3; } }
+
+        [Display(Name = "Product SubDescription Title 4")]
         public string SubDescriptionTitle4 { get { return product.ProductDescription.SubDescriptionTitle4; } }
+
+        [Display(Name = "Product SubDescription 4")]
         public string SubDescription4 { get { return product.ProductDescription.SubDescription4; } }
+
+        [Display(Name = "Product SubDescription Title 5")]
         public string SubDescriptionTitle5 { get { return product.ProductDescription.SubDescriptionTitle5; } }
+
+        [Display(Name = "Product SubDescription 5")]
         public string SubDescription5 { get { return product.ProductDescription.SubDescription5; } }
 
         public ProductDetails (Product prod, IEnumerable<ProductGame> games, IEnumerable<ProductCategory> categories)
