c3dc39ef8c67929b63bd973d3a4c15349d38fedc
Dancelaim <49374348+Dancelaim@users.noreply.github.com>
2020-11-24 02:41:18 +0500

    Db Option fix

---
+++ Domain/ScriptHistory/32.sql
@@ -0,0 +1,21 @@
+﻿Begin Try
+    SET NOCOUNT ON
+    Set XACT_ABORT ON
+Begin Tran
+exec Check_db_Version 32
+
+ALTER TABLE ProductOptions
+ADD OptionProductId Uniqueidentifier
+
+ALTER TABLE ProductOptions
+ADD CONSTRAINT FK_ProductOptions_Product FOREIGN KEY (OptionProductId) REFERENCES Product(ProductId)
+
+drop table ProductToOption
+
+exec Update_db_Version 32
+Commit Tran
+End Try
+Begin Catch
+   RollBack Tran
+   Select ERROR_MESSAGE() as Msg, ERROR_NUMBER() as Num
+End Catch
