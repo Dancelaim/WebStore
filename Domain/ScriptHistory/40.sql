7f0e19896dc2d4b26554d62f0a255b0f128ee4ae
dancelaim <dancelaim@gmail.com>
2021-02-08 21:18:06 +0500

    Article Update

---
+++ Domain/ScriptHistory/40.sql
@@ -0,0 +1,16 @@
+﻿Begin Try
+    SET NOCOUNT ON
+    Set XACT_ABORT ON
+Begin Tran
+exec Check_db_Version 40
+
+ALTER TABLE Article 
+ALTER COLUMN ArticlePostTime DATETIME NOT NULL 
+
+exec Update_db_Version 40
+Commit Tran
+End Try
+Begin Catch
+   RollBack Tran
+   Select ERROR_MESSAGE() as Msg, ERROR_NUMBER() as Num
+End Catch
