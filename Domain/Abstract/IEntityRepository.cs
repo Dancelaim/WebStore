dac3754124d45b174d5934714895b530ffd981c0
Myownjormungand@gmail.com <Myownjormungand@gmail.com>
2021-02-16 01:11:46 +0500

    ProductSubCategory Save/Edit

--- Domain/Abstract/IEntityRepository.cs
+++ Domain/Abstract/IEntityRepository.cs
@@ -43,7 +43,7 @@ namespace WowCarry.Domain.Abstract
         void SaveOrders(OrderDetails orderDetails);
         void SaveArticle(ArticleDetails articleDetails);
         void SaveProductCategory(ProductCategoryDetails productCategoryDetails);
-        void SaveSaveProductSubCategory(ProductSubCategoryDetails productSubCategoryDetails);
+        void SaveProductSubCategory(ProductSubCategoryDetails productSubCategoryDetails);
         void RemoveHtmlBlock(Guid htmlBlockId);
     }
 }
